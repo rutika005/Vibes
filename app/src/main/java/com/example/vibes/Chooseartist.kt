4cf148c465b28c63e88a588651f3417d978cf3ee
rutika005 <134037361+rutika005@users.noreply.github.com>
2024-09-03 11:38:40 +0530

    - add grid view

--- app/src/main/java/com/example/vibes/Chooseartist.kt
+++ app/src/main/java/com/example/vibes/Chooseartist.kt
@@ -1,20 +1,56 @@
 package com.example.vibes
 
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.vibes.adapter.GridAdapter
 
 class Chooseartist : AppCompatActivity() {
+
+    lateinit var gridView: GridView
+    private var numberNames = arrayOf(
+        "One", "Two", "Three", "Four", "Five",
+        "Six", "Seven", "Eight", "Nine", "Ten"
+    )
+    private var numberImages = intArrayOf(
+        R.drawable.atif_aslam,
+        R.drawable.kk,
+        R.drawable.atif_aslam,
+        R.drawable.kk,
+        R.drawable.atif_aslam,
+        R.drawable.kk,
+        R.drawable.atif_aslam,
+        R.drawable.kk,
+        R.drawable.atif_aslam,
+        R.drawable.kk,
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_chooseartist)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        title = "Grid View"
+
+        gridView = findViewById(R.id.gridView)
+
+        val mainAdapter = GridAdapter(
+            context = this,
+            numbersInWords = numberNames,
+            numberImage = numberImages
+        )
+
+        gridView.adapter = mainAdapter
+        gridView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(
+                applicationContext, "You CLicked " + numberNames[+position],
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
