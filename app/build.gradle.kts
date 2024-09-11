9392d8b8a72638ba423ec58d6aa448da2ccdbadc
rutika005 <134037361+rutika005@users.noreply.github.com>
2024-09-11 18:31:04 +0530

    - update activity_musictype.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.android.support:percent:25.3.0")
 }
