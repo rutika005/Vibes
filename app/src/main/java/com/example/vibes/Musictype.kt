705849dcea7272cb075c06a4b677022f52e01526
rutika005 <134037361+rutika005@users.noreply.github.com>
2024-09-12 20:46:00 +0530

    - update activity_musictype.xml

--- app/src/main/java/com/example/vibes/Musictype.kt
+++ app/src/main/java/com/example/vibes/Musictype.kt
@@ -1,6 +1,8 @@
 package com.example.vibes
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,15 @@ class Musictype : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_musictype)
+
+        val iconback = findViewById<Button>(R.id.iconback)
+
+
+
+        iconback.setOnClickListener{
+            val i = Intent(this,Chooseartist::class.java)
+            startActivity(i)
+        }
 
 
     }
