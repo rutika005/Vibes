5622e19de072df247554b0519f7b0afbdb557396
rutika005 <134037361+rutika005@users.noreply.github.com>
2024-09-27 20:29:41 +0530

    add some files includes Fragment files

--- app/src/main/java/com/example/vibes/activity/Languagechoose.kt
+++ app/src/main/java/com/example/vibes/activity/Languagechoose.kt
@@ -1,17 +1,34 @@
 package com.example.vibes.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.Chip
 import com.example.vibes.R
+import com.google.android.material.chip.ChipGroup
 
 class Languagechoose : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_languagechoose)
-        
+
+        val ic_back = findViewById<ImageButton>(R.id.ic_back)
+
+        ic_back.setOnClickListener{
+            val i = Intent(this, Musictype::class.java)
+            startActivity(i)
+        }
+
+        val chipGroupChoice = findViewById<ChipGroup>(R.id.ChipGroup)
+        chipGroupChoice.setOnCheckedChangeListener { group, checkedId ->
+            val chip: Chip? = group.findViewById(checkedId)
+            chip?.let { chipView ->
+                Toast.makeText(this, chipView.text, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
